f5fa161e1679262d7aea1d4b6690b0309cf9a3be
RuiCaiNLP <Rui.Cai@ed.ac.uk>
2019-03-05 11:17:15 +0000

    files

---
+++ CoNLL09-SRL/preprocess.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+python preprocess-conll09.py \
+--train CoNLL2009-ST-English-train.txt \
+--test CoNLL2009-ST-evaluation-English.txt \
+--dev CoNLL2009-ST-English-development.txt
